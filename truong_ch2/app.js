2da5472c25017f3605f3a0d5edca45ecc061e014
Truong Nguyen <nctruong861990@gmail.com>
2016-05-09 00:18:20 +0700

    INIT

---
+++ truong_ch2/app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+
+var http = require('http');
+var path = require('path');
+
+var app = express();
+ 
+// configure Express.js settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+
+// Define routes
+app.all('*', function(req, res) { // catch all request
+  res.render('index', {msg: 'Welcome to the Practical Node.js!'})
+})
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
